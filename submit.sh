a592a61675ef5dfa2fac440bc00297cb07877175
Ryushi-tech <mryrys@gmail.com>
2021-05-26 09:18:03 +0900

    Update to simplify submit.sh

--- submit.sh
+++ submit.sh
@@ -3,50 +3,21 @@
 home=/Users/ryushi/CLionProjects/factory/
 
 cd ${home}A || exit
-oj t
-if [ $? = 0 ]; then
-  oj s -w 0 main.cpp --no-open
-else
-  echo "skip"
-fi
+if oj t; then oj s -w 0 main.cpp --no-open; else echo "#test failed and submission skipped"; fi
 
 cd ${home}B || exit
-oj t
-if [ $? = 0 ]; then
-  oj s -w 0 main.cpp --no-open
-else
-  echo "skip"
-fi
+if oj t; then oj s -w 0 main.cpp --no-open; else echo "#test failed and submission skipped"; fi
+
 cd ${home}C || exit
-oj t
-if [ $? = 0 ]; then
-  oj s -w 0 main.cpp --no-open
-else
-  echo "skip"
-fi
+if oj t; then oj s -w 0 main.cpp --no-open; else echo "#test failed and submission skipped"; fi
 
 cd ${home}D || exit
-oj t
-if [ $? = 0 ]; then
-  oj s -w 0 main.cpp --no-open
-else
-  echo "skip"
-fi
+if oj t; then oj s -w 0 main.cpp --no-open; else echo "#test failed and submission skipped"; fi
 
 cd ${home}E || exit
-oj t
-if [ $? = 0 ]; then
-  oj s -w 0 main.cpp --no-open
-else
-  echo "skip"
-fi
+if oj t; then oj s -w 0 main.cpp --no-open; else echo "#test failed and submission skipped"; fi
 
 cd ${home}F || exit
-oj t
-if [ $? = 0 ]; then
-  oj s -w 0 main.cpp --no-open
-else
-  echo "skip"
-fi
+if oj t; then oj s -w 0 main.cpp --no-open; else echo "#test failed and submission skipped"; fi
 
 cd ${home} || exit
