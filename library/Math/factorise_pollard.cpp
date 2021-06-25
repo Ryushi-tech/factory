7d3bfdb0baa198ad34c3109f0d08f9bab0373288
Ryushi-tech <mryrys@gmail.com>
2021-06-25 10:13:44 +0900

    Add factorise_pollard.cpp

---
+++ library/Math/factorise_pollard.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define rrep(i,n) for(int i=(n)-1;i>=0;i--)
+#define rep1(i,n) for(int i=1;i<=(n);i++)
+#define rrep1(i,n) for(int i=(n);i>0;i--)
+#define fore(i_in,a) for (auto& i_in: a)
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define cnts(x,c) count(all(x),c)
+#define fio() cin.tie(nullptr);ios::sync_with_stdio(false);
+#define DEBUG_CTN(v) cerr<<#v<<":";for(auto itr=v.begin();itr!=v.end();itr++) cerr<<" "<<*itr; cerr<<endl
+template<class T> bool chmax(T &a, const T &b) {if (a<b) {a = b; return 1;} return 0;}
+template<class T> bool chmin(T &a, const T &b) {if (a>b) {a = b; return 1;} return 0;}
+template<class T> void print(const T &t) {cout<<t<<"\n";}
+template<class T> void PRINT(const T &t) {rep(i,sz(t)) cout<<t[i]<<" \n"[i==sz(t)-1];}
+const ll INF = 1LL << 62;
+const int iINF = 1 << 30;
+
+typedef unsigned long long ull;
+ull modmul(ull a, ull b, ull M) {
+    ll ret = a * b - M * ull(1.L / M * a * b);
+    return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+
+ull modpow(ull b, ull e, ull mod) {
+    ull ans = 1;
+    for (; e; b = modmul(b, b, mod), e /= 2)
+        if (e & 1) ans = modmul(ans, b, mod);
+    return ans;
+}
+
+bool isPrime(ull n) {
+    if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+    ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+            s = __builtin_ctzll(n-1), d = n >> s;
+    for (ull a : A) {   // ^ count trailing zeroes
+        ull p = modpow(a%n, d, n), i = s;
+        while (p != 1 && p != n - 1 && a % n && i--)
+            p = modmul(p, p, n);
+        if (p != n-1 && i != s) return 0;
+    }
+    return 1;
+}
+
+ull pollard(ull n) {
+    auto f = [n](ull x) { return modmul(x, x, n) + 1; };
+    ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+    while (t++ % 40 || __gcd(prd, n) == 1) {
+        if (x == y) x = ++i, y = f(x);
+        if ((q = modmul(prd, max(x,y) - min(x,y), n))) prd = q;
+        x = f(x), y = f(f(y));
+    }
+    return __gcd(prd, n);
+}
+
+vector<ull> factor(ull n) {
+    if (n == 1) return {};
+    if (isPrime(n)) return {n};
+    ull x = pollard(n);
+    auto l = factor(x), r = factor(n / x);
+    l.insert(l.end(), all(r));
+    return l;
+}
+
+ull n;
+
+int main() {
+    fio(); cin>>n;
+    auto res=factor(n);
+    int obj=sz(res),cnt=0,base=1;
+    while(base<obj){
+        base*=2;
+        cnt++;
+    }
+    print(cnt);
+}
+//typical90-75
