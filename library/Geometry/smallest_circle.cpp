b3298d27f37e8580459ed82c45ffb822c11fcc83
Ryushi-tech <mryrys@gmail.com>
2022-02-11 09:28:25 +0900

    Add smallest_circle.cpp

---
+++ library/Geometry/smallest_circle.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using vl = vector<ll>;
+using vi = vector<int>;
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define rrep(i,n) for(int i=(n)-1;i>=0;i--)
+#define rep1(i,n) for(int i=1;i<=(n);i++)
+#define rrep1(i,n) for(int i=(n);i>0;i--)
+#define fore(i_in,a) for (auto& i_in: a)
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define cnts(x,c) count(all(x),c)
+#define fio() cin.tie(nullptr);ios::sync_with_stdio(false);
+#define DEBUG_CTN(v) cerr<<#v<<":";for(auto itr=v.begin();itr!=v.end();itr++) cerr<<" "<<*itr; cerr<<endl
+template<class T> bool chmax(T &a, const T &b) {if (a<b) {a = b; return true;} return false;}
+template<class T> bool chmin(T &a, const T &b) {if (a>b) {a = b; return true;} return false;}
+template<class T> void print(const T &t) {cout<<t<<"\n";}
+template<class T> void PRINT(const T &t) {rep(i,sz(t)) cout<<t[i]<<" \n"[i==sz(t)-1];}
+const ll INF = 1LL << 62;
+const int iINF = 1 << 30;
+
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+template<class T>
+struct Point {
+    typedef Point P;
+    T x, y;
+    explicit Point(T x=0, T y=0) : x(x), y(y) {}
+    bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+    bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+    P operator+(P p) const { return P(x+p.x, y+p.y); }
+    P operator-(P p) const { return P(x-p.x, y-p.y); }
+    P operator*(T d) const { return P(x*d, y*d); }
+    P operator/(T d) const { return P(x/d, y/d); }
+    T dot(P p) const { return x*p.x + y*p.y; }
+    T cross(P p) const { return x*p.y - y*p.x; }
+    T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+    T dist2() const { return x*x + y*y; }
+    double dist() const { return sqrt((double)dist2()); }
+    // angle to x-axis in interval [-pi, pi]
+    double angle() const { return atan2(y, x); }
+    P unit() const { return *this/dist(); } // makes dist()=1
+    P perp() const { return P(-y, x); } // rotates +90 degrees
+    P normal() const { return perp().unit(); }
+    // returns point rotated 'a' radians ccw around the origin
+    P rotate(double a) const {
+        return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+    friend ostream& operator<<(ostream& os, P p) {
+        return os << "(" << p.x << "," << p.y << ")"; }
+};
+
+typedef Point<double> P;
+double ccRadius(const P& A, const P& B, const P& C) {
+    return (B-A).dist()*(C-B).dist()*(A-C).dist()/
+           abs((B-A).cross(C-A))/2;
+}
+P ccCenter(const P& A, const P& B, const P& C) {
+    P b = C-A, c = B-A;
+    return A + (b*c.dist2()-c*b.dist2()).perp()/b.cross(c)/2;
+}
+
+int n;
+P p[55];
+double ret;
+
+void hoge(P x){
+    double r=0;
+    rep(i,n) chmax(r,(p[i]-x).dist());
+    chmin(ret,r);
+}
+
+int main() {
+    fio(); cin>>n;
+    rep(i,n) cin>>p[i].x>>p[i].y;
+    ret=1e9;
+    rep(z,n) rep(y,z){
+        P c=(p[z]+p[y])/2;
+        hoge(c);
+        rep(x,y){
+            if(isinf(ccRadius(p[x],p[y],p[z]))) continue;
+            P d=ccCenter(p[x],p[y],p[z]);
+            hoge(d);
+        }
+    }
+    cout << fixed << setprecision(12) << ret << "\n";
+}
+//enclose all
