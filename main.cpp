7edfbabf14f395ef8d80b867bc7faa0b49e245d3
Ryushi-tech <mryrys@gmail.com>
2023-04-16 16:24:08 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,44 +24,21 @@ template<class T> void print(const T &t) {cout<<t<<"\n";}
 template<class T> void PRINT(const T &t) {rep(i,sz(t)) cout<<t[i]<<" \n"[i==sz(t)-1];}
 const ll INF = 0x3f3f3f3f3f3f3f3fll;
 const int iINF = 1 << 30;
+int n;
+string s;
 
-ll h, w;
-ll r[2005],c[2005];
-char a[2005][2005];
-ll dp[2005][2005][2][2];
-ll di[2]={1,0},dj[2]={0,1};
-
-ll solve(){
-    rep1(i,h) rep1(j,w) rep(d,2) rep(f,2) dp[i][j][d][f]=INF;
-    rep(r1,2){
-        int c1=a[1][1]^r1;
-        dp[1][1][0][c1]=r1*r[1]+c1*c[1];
-        dp[1][1][1][r1]=r1*r[1]+c1*c[1];
-    }
-    rep1(i,h) rep1(j,w) rep(d,2) rep(l,2){
-        int ni=i+di[d],nj=j+dj[d];
-        if(ni>h or nj>w) continue;
-        int flip=a[ni][nj]^l,cost=0;
-        if(d==0) cost=r[ni]*flip;
-        else cost=c[nj]*flip;
-        rep(nd,2){
-            int nl=l;
-            if(d!=nd) nl=flip;
-            chmin(dp[ni][nj][nd][nl],dp[i][j][d][l]+cost);
+int main(){
+    fio(); cin>>n>>s;
+    int x=0,y=0;
+    int cur=0;
+    fore(t,s){
+        if(t=='R') cur++;
+        else{
+            if(cur%4==0) x++;
+            if(cur%4==1) y--;
+            if(cur%4==2) x--;
+            if(cur%4==3) y++;
         }
     }
-    ll ret=INF;
-    rep(d,2) rep(f,2) chmin(ret,dp[h][w][d][f]);
-    return ret;
-}
-
-int main() {
-    fio();cin>>h>>w;
-    rep1(i,h) cin>>r[i];
-    rep1(i,w) cin>>c[i];
-    rep1(i,h) rep1(j,w) cin>>a[i][j],a[i][j]-='0';
-    ll ans=solve();
-    rep1(i,h) rep1(j,w) a[i][j]^=1;
-    chmin(ans,solve());
-    print(ans);
+    cout<<x<<" "<<y<<endl;
 }
